5e5484b2c23bb74125ab0a5a01efdad7fe922ca4
Jonathan G. Marzurkiewicz <peljonas@gmail.com>
2024-06-27 12:05:25 -0400

    implementação da movimentação do jogador

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
-@onready var arms = $texture/arms
-@onready var player_arms = $texture/arms/player_arms
+@onready var anim = $anim
+@onready var arms = $anim/arms
+@onready var player_arms = $anim/arms/player_arms
 
+var move_speed := 230.0
+var acceleration := 0.3
+var friction := 0.25
 
 func _process(delta):
-	#rotate_arms(arms, get_global_mouse_position(), 250.0, delta)
+	move()
 	arms.look_at(get_global_mouse_position())
 	var direction = (get_global_mouse_position() - arms.global_position)
 	if direction.x >= 0:
@@ -13,8 +17,24 @@ func _process(delta):
 	else:
 		arms.scale.y = -0.2
 	
+
+func move():
+	var direction := Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
 	
+	if direction != Vector2.ZERO:
+		velocity.x = lerp(velocity.x, direction.normalized().x * move_speed, acceleration)
+		velocity.y = lerp(velocity.y, direction.normalized().y * move_speed, acceleration)
+		anim.play("walking")
+	else: 
+		velocity.x = lerp(velocity.x, direction.normalized().x * move_speed, friction)
+		velocity.y = lerp(velocity.y, direction.normalized().y * move_speed, friction)
+		anim.play("idle")
 	
+	move_and_slide()
+
 
 #funcao para rotacionar os braços. Muito parecida com a funcao look_at() da prórpia godot, mas aqui é possível definir a velocidade de rotação
 func rotate_arms(self_object, target_global_posiiton: Vector2, armsRotationSpeed, delta):
